58a43a03a1726e7a1b77ac6974405659ed397874
tugumitugutugu <tugu0607.mi@gmail.com>
2026-07-05 23:12:45 +0900

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'つぐぶろぐ';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = '雪国青森で暮らす40代女性つぐみの正直レビューと暮らしのブログ';
